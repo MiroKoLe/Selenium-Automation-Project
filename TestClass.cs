ed7d3351b92a32cb4ea5e85a27a93c3f85bc20b0
ELEKS-SOFTWARE\miroslav.kolev <miroslavkolev@gmail.com>
2024-04-12 10:35:48 +0200

    Project setup and automation login.

---
+++ TestClass.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Selenium_Automation_Project
+{
+    public class TestClass
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [TestMethod]
+        public void LoginTest() 
+        {jhgjghjghjgh
+            var url = "http://saucedemo.com";
+            var expected_url = url;
+            driver.Navigate().GoToUrl(url);
+
+            Assert.AreEqual(expected_url, url);
+
+            var username_input = driver.FindElement(By.Id("user-name"));
+            var password_input = driver.FindElement(By.Id("password"));
+
+            username_input.SendKeys("standard_user");
+            password_input.SendKeys("secret_sauce");
+
+            var login_button = driver.FindElement(By.Id("login-button"));
+            login_button.Click();
+
+        }
+    }
+}
